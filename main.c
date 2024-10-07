522b7faf6703761a3581866901370c2f7431799c
arthurcarrell <Arthurcarrell08@gmail.com>
2024-10-07 19:47:45 +0100

    Updated the prime number checker to use 'unsigned long long int'

--- main.c
+++ main.c
@@ -20,13 +20,13 @@ int stringLength(char *str) // returns the length of a string
 
 
 // MAIN FUNCTIONS
-int quickNumberPrimeCheck(int numberToCheck)
+int quickNumberPrimeCheck(unsigned long long int numberToCheck)
 {
     // simply checks if the number ends with an even number or a 5.
 
     // convert the int to a string
-    char convertedString[20];
-    snprintf(convertedString,sizeof(convertedString), "%d", numberToCheck);
+    char convertedString[30];
+    snprintf(convertedString,sizeof(convertedString), "%llu", numberToCheck);
     
     // convertedString is now the string equivalent of the int.
     char lastChr = convertedString[stringLength(convertedString)-1];
@@ -42,7 +42,7 @@ int quickNumberPrimeCheck(int numberToCheck)
     }
 }
 
-int checkIfPrimeNumber(int numberToCheck)
+long int checkIfPrimeNumber(unsigned long long int numberToCheck)
 {
     int hasFoundFactor = 0;
     int numberToDivide = 2;
@@ -62,14 +62,8 @@ int checkIfPrimeNumber(int numberToCheck)
 }
 
 int main(int argc, char *argv[])
-{ 
-    int numsToCheck[200] = {3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,1153,1163,1171,1181,1187,1193,1201,1213,1217,1223};
-    
-    for (int i=0; i <= 198; i++)
-    {
-        printf("Input: %d -- checkIfPrimeNumber returned: %d\n", numsToCheck[i], checkIfPrimeNumber(numsToCheck[i]));
-    }
-
-    printf("Input: %d -- checkIfPrimeNumber returned: %d\n", 2147483647, checkIfPrimeNumber(2147483647));
+{
+    unsigned long long int checkNum = 18446744073709551615; // unsigned 64-bit largest number
+    printf("Input: %llu -- checkIfPrimeNumber returned: %ld\n", checkNum, checkIfPrimeNumber(checkNum));
     return 1;
 }
